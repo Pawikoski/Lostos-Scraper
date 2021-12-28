a776a911eb499fe096eb91cdd894a8b29e68b45e
Pawel <pawikoski@gmail.com>
2021-12-28 17:17:24 +0100

    Time measuring

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
 
 def scrap(pages:int=1):
     for i in range(pages):
+        
         site = requests.get(f"https://losots.pl/index.php?action=loshub&do=highscores&list=experience&page={i+1}")
 
         bs = BeautifulSoup(site.content, 'html.parser')
@@ -12,6 +14,8 @@ def scrap(pages:int=1):
         print(len(rows))
 
         for row in rows:
+            start_time = time.perf_counter()
+            
             data = row.find_all("td")
             position = data[0].text
             character = data[1].text
@@ -21,10 +25,17 @@ def scrap(pages:int=1):
             experience = int(data[4].text)
 
             print(position, character, "|", vocation, "|", level, "|", experience)
+            time_leaderboard = f"Time: {time.perf_counter() - start_time:0.4f}s (only data from leaderboard)"
             
             equipment = BeautifulSoup(requests.get(profile_link).content, 'html.parser').find_all("table", {"class": "tborder"})[2].find_all("img")
 
-            print(', '.join([eq['src'] for eq in equipment]))
+            print([eq['src'] if ("/10121.gif" or "/5.gif") in eq else None for eq in equipment])
+            print()
+            
+            print(time_leaderboard)
+            print(f"Time with profile scraping: {time.perf_counter() - start_time:0.4f}s")
+
+            print()
             print()
 
 
